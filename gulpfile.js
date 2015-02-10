e179783f00e204f608516bc3bfdaa7013a5ea4ae
xeronimus <xeronimus@gmail.com>
2015-02-10 11:29:48 +0100

    prep for tag "taskautomation"

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+var karma = require('karma').server;
+
+var server = {
+  host: 'localhost',
+  port: '8001',
+  root: './app'
+};
+
+gulp.task('webserver', function () {
+  gulp.src(server.root)
+    .pipe(webserver({
+      host: server.host,
+      port: server.port,
+      open: true,
+      livereload: true,
+      directoryListing: false
+    }));
+});
+
+/**
+ * Runs our unit tests with karma
+ * */
+gulp.task('test', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    autoWatch: false
+  }, done);
+});
+
+/**
+ * Watch for file changes and re-run tests on each change
+ * */
+gulp.task('tdd', function (done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    autoWatch: true
+  }, done);
+});
+
+gulp.task('default', ['webserver', 'tdd']);
